798e40ea162da68da5d4ebe8ce8f7569afd63bb1
Andreas Sundqvist <andreas@stefna.is>
2025-11-05 15:38:57 +0100

    Add raw mode to generating key

--- src/Cli/GenerateSecret/Command.php
+++ src/Cli/GenerateSecret/Command.php
@@ -4,6 +4,7 @@ namespace Starburst\EncryptedConfigLoader\Cli\GenerateSecret;
 
 use ParagonIE\Halite\KeyFactory;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class Command extends \Circli\Console\Definition
@@ -11,10 +12,18 @@ final class Command extends \Circli\Console\Definition
 	protected function configure(): void
 	{
 		$this->setName('config:generate-secret');
+		$this->addOption('raw', '', InputOption::VALUE_NONE);
 		$this->setCommand(function (InputInterface $input, OutputInterface $output) {
-			$output->writeln("<info>Generating secret</info>\n" . PHP_EOL);
+			if (!$input->getOption('raw')) {
+				$output->writeln("<info>Generating secret</info>\n" . PHP_EOL);
+			}
 
 			$key = KeyFactory::export(KeyFactory::generateEncryptionKey());
+
+			if ($input->getOption('raw')) {
+				$output->writeln(sprintf('%s', $key->getString()));
+				return 0;
+			}
 
 			$output->writeln('<info>Secret:</info>');
 			$output->writeln(sprintf('%s', $key->getString()));
